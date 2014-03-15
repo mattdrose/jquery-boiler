4810b1cc7be50df0ffd5402b2233ae0ad9c1abb7
Matt Rose <rose.matt@gmail.com>
2014-03-14 21:06:41 -0400

    Updated plugin name to boiler

---
+++ jquery.boiler.template.js
@@ -0,0 +1,25 @@
+/**
+ * This is a starting template for creating jQuery plugins with jquery.boiler
+ */
+
+$.boiler('pluginName', {
+
+  defaults: {},
+
+  events: {},
+
+  data: [],
+
+  init: function(){
+    var plugin = this;
+	},
+
+  _privateMethod: function(){
+    var plugin = this;
+  },
+
+  publicMethod: function(){
+    var plugin = this;
+  }
+
+});
